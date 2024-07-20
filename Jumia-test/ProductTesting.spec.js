590b31bd5c2a644ba89a759c398a28498439d0dd
KwyTechDev <KwyTechDev@users.noreply.github.com>
2024-07-20 17:14:29 +0000

    deploy: 730fe01d1f8334ee897b1214a24067621b4026c2

--- Jumia-test/ProductTesting.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('ProductTesting', async ({ page }) => {
-  await page.goto('https://www.jumia.com.gh/');
-  await page.getByRole('button', { name: 'newsletter_popup_close-cta' }).click();
-  await page.goto('https://www.jumia.com.gh/db313-2-in-1-blender-blue-delron-mpg1971483.html');
-});
